d766443508982fdc139a2dc74d68a9a3a807c143
aalluinmar <aravind.allu@inmar.com>
2019-12-13 16:01:41 +0530

    added excel to json conversion by using vue-xlsx

--- excel-to-json/src/components/HelloWorld.vue
+++ excel-to-json/src/components/HelloWorld.vue
@@ -1,32 +1,84 @@
 <template>
-  <div class="hello">
-    <h1>Excel to Json Conversion</h1>
+  <div>
+    <section>
+      <h1>Create XLSX</h1>
+      <div>
+        <input v-model="sheetName" placeholder="type a new sheet name" />
+        <button v-if="sheetName" @click="addSheet">Add Sheet</button>
+      </div>
+
+      <div>Sheets: {{ sheets }}</div>
+
+      <xlsx-workbook>
+        <xlsx-sheet
+          :collection="sheet.data"
+          v-for="sheet in sheets"
+          :key="sheet.name"
+          :sheet-name="sheet.name"
+        />
+        <xlsx-download>
+          <button>Download</button>
+        </xlsx-download>
+      </xlsx-workbook>
+    </section>
+    <hr />
+    <section>
+      <h3>Import XLSX</h3>
+      <input type="file" @change="onChange" />
+      <xlsx-read :file="file">
+        <xlsx-sheets>
+          <template #default="{sheets}">
+            <select v-model="selectedSheet">
+              <option v-for="sheet in sheets" :key="sheet" :value="sheet">
+                {{ sheet }}
+              </option>
+            </select>
+          </template>
+        </xlsx-sheets>
+        <xlsx-table :sheet="selectedSheet" />
+        <xlsx-json :sheet="selectedSheet">
+          <template #default="{collection}">
+            <div>
+              {{ collection }}
+            </div>
+          </template>
+        </xlsx-json>
+      </xlsx-read>
+    </section>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+/* eslint-disable */
+import { XlsxRead, XlsxTable, XlsxSheets, XlsxJson, XlsxWorkbook, XlsxSheet, XlsxDownload } from "../../node_modules/vue-xlsx/dist/vue-xlsx.es.js"
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+export default {
+  components: {
+    XlsxRead,
+    XlsxTable,
+    XlsxSheets,
+    XlsxJson,
+    XlsxWorkbook,
+    XlsxSheet,
+    XlsxDownload
+  },
+  data() {
+    return {
+      file: null,
+      selectedSheet: null,
+      sheetName: null,
+      sheets: [{ name: "SheetOne", data: [{ c: 2 }] }],
+      collection: [{ a: 1, b: 2 }]
+    };
+  },
+  methods: {
+    onChange(event) {
+      this.file = event.target.files ? event.target.files[0] : null;
+    },
+    addSheet() {
+      this.sheets.push({ name: this.sheetName, data: [...this.collection] });
+      this.sheetName = null;
+    }
+  }
+};
+</script>
