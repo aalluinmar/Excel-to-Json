baa5b1b26c7e322814b19f08cc891977842e496d
aalluinmar <aravind.allu@inmar.com>
2019-12-13 17:01:01 +0530

    modified XlsxJson also

--- excel-to-json/src/components/HelloWorld.vue
+++ excel-to-json/src/components/HelloWorld.vue
@@ -1,21 +1,42 @@
 <template>
-  <div>
-      <h3>Import XLSX</h3>
-      <input type="file" @change="onChange" />
-      <xlsx-read :file="file">
-        <xlsx-table />
-      </xlsx-read>
+  <div class="container-fluid bb-1">
+    <div class="row">
+      <div class="col-xl">
+        <h3>Import XLSX</h3>
+      </div>
+    </div>
+    <br>
+    <div class="row">
+      <div class="col-12">
+        <div class="col-md-12">
+          <label>Choose an Excel file: </label>&nbsp;
+          <input type="file" @change="onChange" />
+        </div>
+      </div>
+    </div>
+    <xlsx-read :file="file">
+      <xlsx-table />
+      <xlsx-json>
+        <template #default="{collection}">
+          <div>
+            {{ collection }}
+          </div>
+        </template>
+      </xlsx-json>
+    </xlsx-read>
+
   </div>
 </template>
 
 <script>
 /* eslint-disable */
-import { XlsxRead, XlsxTable } from "../../node_modules/vue-xlsx/dist/vue-xlsx.es.js"
+import { XlsxRead, XlsxTable, XlsxJson } from "../../node_modules/vue-xlsx/dist/vue-xlsx.es.js"
 
 export default {
   components: {
     XlsxRead,
-    XlsxTable
+    XlsxTable,
+    XlsxJson
   },
   data() {
     return {
